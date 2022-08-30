f1b0b0c3ca8dbb08669d5066030f035cc32f2e95
Islombek Turakulov <iuturakulov@edu.hse.ru>
2022-08-30 10:28:25 +0300

    Layout updates.

--- app/src/main/java/com/iuturakulov/uzbarchitecture_ar/ui/activities/MainActivity.kt
+++ app/src/main/java/com/iuturakulov/uzbarchitecture_ar/ui/activities/MainActivity.kt
@@ -5,21 +5,12 @@ import androidx.activity.viewModels
 import androidx.annotation.VisibleForTesting
 import com.iuturakulov.uzbarchitecture_ar.R
 import com.iuturakulov.uzbarchitecture_ar.databinding.ActivityMainBinding
-import com.iuturakulov.uzbarchitecture_ar.network.ArchitectureClient
 import com.iuturakulov.uzbarchitecture_ar.network.NetworkConnectivityObserver
-import com.iuturakulov.uzbarchitecture_ar.storage.ArchitectureInfoDao
 import com.iuturakulov.uzbarchitecture_ar.ui.adapter.ArchitectureAdapter
 import com.iuturakulov.uzbarchitecture_ar.ui.viewmodel.MainViewModel
 import com.skydoves.bindables.BindingActivity
-import com.skydoves.sandwich.message
-import com.skydoves.sandwich.suspendOnError
-import com.skydoves.sandwich.suspendOnSuccess
 import com.skydoves.transformationlayout.onTransformationStartContainer
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,12 +18,6 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
 
     @VisibleForTesting
     val viewModel: MainViewModel by viewModels()
-
-    @Inject
-    lateinit var architectureClient: ArchitectureClient
-
-    @Inject
-    lateinit var architectureInfoDao: ArchitectureInfoDao
 
     @Inject
     lateinit var connectivityObserver: NetworkConnectivityObserver
@@ -46,21 +31,7 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
             adapter = ArchitectureAdapter()
             vm = viewModel
         }
-    }
 
-    override fun onStart() {
-        super.onStart()
-        CoroutineScope(Dispatchers.IO).launch {
-            val response = architectureClient.fetchArchitectureInfo()
-            response.suspendOnSuccess {
-                data.forEach {
-                    if (architectureInfoDao.getArchitecture(it.id) == null) {
-                        architectureInfoDao.insertArchitectureInfo(it)
-                    }
-                }
-            }.suspendOnError {
-                Timber.d(message())
-            }
-        }
+
     }
 }
